1cb63cd48648bba1a35d425a19edf67e2d0c2709
Maithuong <Maithuong@172.21.2.192>
2016-04-19 12:55:05 +0300

    My commit

---
+++ Supertest/src/edu/ifmo/Firstexercise.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.ifmo;
+
+/**
+ * @author Maithuong
+ *
+ */
+public class Firstexercise {
+	public static void main(String[] args){
+        double n=8.5;
+        double m=11.45;
+        int a=10;
+        double b=Math.abs(a-n);
+        double c=Math.abs(a-m);  
+        if (b==c){
+            System.out.println("Числа "+n +" и "+m + " равноудалены от "+a);
+        }
+        if(b>c){
+            System.out.println("Число "+m+" ближе к "+a);
+        }
+        if (b<c) {
+            System.out.println("Число "+n+" ближе к "+a);
+        }
+    }
+
+}
