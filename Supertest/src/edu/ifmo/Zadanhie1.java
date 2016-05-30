c9edca1302fc24b4b386ed54923772cf438dc2ab
Maithuong <Maithuong@maithuong-pc>
2016-05-30 23:38:05 +0300

    zadanhie1

---
+++ Supertest/src/edu/ifmo/Zadanhie1.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package edu.ifmo;
+
+/**
+ * @author Maithuong
+ *
+ */
+public class Zadanhie1 {
+	public static void main(String[] args){
+		int i = 5, j = 10;
+		String str = "My first string";
+		System.out.println(str);
+		System.out.println("Some text:"+str);
+		int k = i+j;
+		System.out.println("Sum:"+k);
+		System.out.println("Sum:"+(i+j));
+		while(i<j){
+			System.out.println(str);
+				i++;
+				}
+		if(i==j){
+			System.out.println("i="+i+", j="+j+" и они равны");
+			} else {
+				System.out.println("i="+i+", j="+j+" и они не равны");
+			}
+
+	}
+}
